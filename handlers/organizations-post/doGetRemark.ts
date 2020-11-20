ecd85052f280c4939fba52fbc2452ca454d7eb4a
dangowans <d.gowans@cityssm.on.ca>
2020-11-20 15:19:09 -0500

    Update doGetRemark.ts

--- handlers/organizations-post/doGetRemark.ts
+++ handlers/organizations-post/doGetRemark.ts
@@ -9,5 +9,4 @@ export const handler: RequestHandler = (req, res) => {
   const remarkIndex = req.body.remarkIndex;
 
   res.json(getOrganizationRemark(organizationID, remarkIndex, req.session));
-
 };
