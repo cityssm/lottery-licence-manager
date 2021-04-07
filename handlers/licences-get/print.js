c518a9f04096cc9302e4a6f82ed8a8c430ceb6ba
dangowans <d.gowans@cityssm.on.ca>
2021-04-07 15:53:12 -0400

    compile server code to es2017

--- handlers/licences-get/print.js
+++ handlers/licences-get/print.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const path = require("path");
@@ -16,7 +7,7 @@ const configFns = require("../../helpers/configFns");
 const licencesDB_getOrganization = require("../../helpers/licencesDB/getOrganization");
 const licencesDB_getLicence = require("../../helpers/licencesDB/getLicence");
 const convertHTMLToPDF = require("pdf-puppeteer");
-const handler = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+const handler = async (req, res, next) => {
     const urlPrefix = configFns.getProperty("reverseProxy.urlPrefix");
     const licenceID = parseInt(req.params.licenceID, 10);
     const licence = licencesDB_getLicence.getLicence(licenceID, req.session);
@@ -29,11 +20,11 @@ const handler = (req, res, next) => __awaiter(void 0, void 0, void 0, function* 
         return;
     }
     const organization = licencesDB_getOrganization.getOrganization(licence.organizationID, req.session);
-    yield ejs.renderFile(path.join(__dirname, "../../reports/", configFns.getProperty("licences.printTemplate")), {
+    await ejs.renderFile(path.join(__dirname, "../../reports/", configFns.getProperty("licences.printTemplate")), {
         configFns,
         licence,
         organization
-    }, {}, (ejsErr, ejsData) => __awaiter(void 0, void 0, void 0, function* () {
+    }, {}, async (ejsErr, ejsData) => {
         if (ejsErr) {
             return next(ejsErr);
         }
@@ -43,12 +34,12 @@ const handler = (req, res, next) => __awaiter(void 0, void 0, void 0, function* 
             res.setHeader("Content-Type", "application/pdf");
             res.send(pdf);
         };
-        yield convertHTMLToPDF(ejsData, pdfCallbackFn, {
+        await convertHTMLToPDF(ejsData, pdfCallbackFn, {
             format: "Letter",
             printBackground: true,
             preferCSSPageSize: true
         });
         return null;
-    }));
-});
+    });
+};
 exports.handler = handler;
