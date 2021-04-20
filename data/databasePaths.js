9fefc2b8ce82bcc06cef11ceaddd561fe25c0b12
dangowans <d.gowans@cityssm.on.ca>
2021-04-20 15:08:59 -0400

    fix export

--- data/databasePaths.js
+++ data/databasePaths.js
@@ -1,5 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usersDB = exports.licencesDB = void 0;
-exports.licencesDB = "data/licences.db";
-exports.usersDB = "data/users.db";
+export const licencesDB = "data/licences.db";
+export const usersDB = "data/users.db";
