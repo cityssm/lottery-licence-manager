0d9bd7fd9c374e0d130bc483cd5dc53aae3b40a4
Dan Gowans <d.gowans@cityssm.on.ca>
2022-10-27 15:04:21 -0400

    prettier

--- data/databasePaths.ts
+++ data/databasePaths.ts
@@ -6,15 +6,14 @@ const debug = Debug("lottery-licence-manager:databasePaths");
 // Determine if test databases should be used
 
 export const useTestDatabases =
-  configFunctions.getProperty("application.useTestDatabases") || process.env.TEST_DATABASES === "true";
+    configFunctions.getProperty("application.useTestDatabases") ||
+    process.env.TEST_DATABASES === "true";
 
 if (useTestDatabases) {
-  debug("Using \"-testing\" databases.");
+    debug('Using "-testing" databases.');
 }
 
 export const licencesDB_live = "data/licences.db";
 export const licencesDB_testing = "data/licences-testing.db";
 
-export const licencesDB = useTestDatabases
-  ? licencesDB_testing
-  : licencesDB_live;
+export const licencesDB = useTestDatabases ? licencesDB_testing : licencesDB_live;
