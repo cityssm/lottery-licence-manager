4ecb1c9938ba3e23515277aed3983114b5f04147
Dan Gowans <dangowans@users.noreply.github.com>
2024-10-09 12:58:32 -0400

    simplify test

--- cypress/e2e/xx-other/loginPage.cy.ts
+++ cypress/e2e/xx-other/loginPage.cy.ts
@@ -13,17 +13,11 @@ describe('Login Page', () => {
 
   it('Contains a login form', () => {
     cy.get('form').should('have.length', 1)
-  })
 
-  it('Contains a _csrf field', () => {
     cy.get("form [name='_csrf']").should('exist')
-  })
 
-  it('Contains a userName field', () => {
     cy.get("form [name='userName']").should('exist')
-  })
 
-  it('Contains a password field', () => {
     cy.get("form [name='password']")
       .should('have.length', 1)
       .invoke('attr', 'type')
