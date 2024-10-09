3f1c89d5c613fdfd97a015952c9cbfa4368eeb54
Dan Gowans <dangowans@users.noreply.github.com>
2024-10-09 11:44:03 -0400

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,47 @@
-import gulp from "gulp";
-import changed from "gulp-changed";
-import concat from "gulp-concat";
-import minify from "gulp-minify";
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
+import gulp from 'gulp';
+import changed from 'gulp-changed';
+import concat from 'gulp-concat';
+import minify from 'gulp-minify';
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
 const sass = gulpSass(dartSass);
-const publicSCSSDestination = "public/stylesheets";
-const publicSCSSFunction = () => {
+const publicSCSSDestination = 'public/stylesheets';
+function publicSCSSFunction() {
     return gulp
-        .src("public-scss/*.scss")
-        .pipe(sass({ outputStyle: "compressed", includePaths: ['node_modules'] }).on("error", sass.logError))
+        .src('public-scss/*.scss')
+        .pipe(sass({ outputStyle: 'compressed', includePaths: ['node_modules'] }).on('error', sass.logError))
         .pipe(gulp.dest(publicSCSSDestination));
-};
-gulp.task("public-scss", publicSCSSFunction);
-const publicJavascriptsDestination = "public/javascripts";
-const publicJavascriptsMinFunction = () => {
-    return gulp.src("public-typescript/*.js", { allowEmpty: true })
+}
+gulp.task('public-scss', publicSCSSFunction);
+const publicJavascriptsDestination = 'public/javascripts';
+function publicJavascriptsMinFunction() {
+    return gulp
+        .src('public-typescript/*.js', { allowEmpty: true })
         .pipe(changed(publicJavascriptsDestination, {
-        extension: ".min.js"
+        extension: '.min.js'
     }))
-        .pipe(minify({ noSource: true, ext: { min: ".min.js" } }))
+        .pipe(minify({ noSource: true, ext: { min: '.min.js' } }))
         .pipe(gulp.dest(publicJavascriptsDestination));
-};
-const publicJavascriptsLicenceEditFunction = () => {
-    return gulp.src([
-        "public-typescript/licence-edit/main.js",
-        "public-typescript/licence-edit/ticketTypes.js"
+}
+function publicJavascriptsLicenceEditFunction() {
+    return gulp
+        .src([
+        'public-typescript/licence-edit/main.js',
+        'public-typescript/licence-edit/ticketTypes.js'
     ], { allowEmpty: true })
-        .pipe(concat("licence-edit.js"))
-        .pipe(minify({ noSource: true, ext: { min: ".min.js" } }))
-        .pipe(gulp.dest("public/javascripts"));
-};
-gulp.task("public-javascript-min", publicJavascriptsMinFunction);
-gulp.task("public-javascript-licence-edit-min", publicJavascriptsLicenceEditFunction);
-const watchFunction = () => {
-    gulp.watch("public-scss/*.scss", publicSCSSFunction);
-    gulp.watch("public-typescript/*.js", publicJavascriptsMinFunction);
-    gulp.watch("public-typescript/licence-edit/*.js", publicJavascriptsLicenceEditFunction);
-};
-gulp.task("watch", watchFunction);
-gulp.task("default", () => {
+        .pipe(concat('licence-edit.js'))
+        .pipe(minify({ noSource: true, ext: { min: '.min.js' } }))
+        .pipe(gulp.dest('public/javascripts'));
+}
+gulp.task('public-javascript-min', publicJavascriptsMinFunction);
+gulp.task('public-javascript-licence-edit-min', publicJavascriptsLicenceEditFunction);
+function watchFunction() {
+    gulp.watch('public-scss/*.scss', publicSCSSFunction);
+    gulp.watch('public-typescript/*.js', publicJavascriptsMinFunction);
+    gulp.watch('public-typescript/licence-edit/*.js', publicJavascriptsLicenceEditFunction);
+}
+gulp.task('watch', watchFunction);
+gulp.task('default', () => {
     publicSCSSFunction();
     publicJavascriptsMinFunction();
     watchFunction();
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,79 +1,95 @@
-/* eslint-disable node/no-unpublished-import */
+// eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, n/no-unpublished-import */
 
-import gulp from "gulp";
-import changed from "gulp-changed";
-import concat from "gulp-concat";
-import minify from "gulp-minify";
+import gulp from 'gulp'
+import changed from 'gulp-changed'
+import concat from 'gulp-concat'
+import minify from 'gulp-minify'
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
 
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
-const sass = gulpSass(dartSass);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const sass = gulpSass(dartSass)
 
 /*
  * Compile SASS
  */
 
-const publicSCSSDestination = "public/stylesheets";
+const publicSCSSDestination = 'public/stylesheets'
 
-const publicSCSSFunction = () => {
-    return gulp
-        .src("public-scss/*.scss")
-        .pipe(sass({ outputStyle: "compressed", includePaths: ['node_modules'] }).on("error", sass.logError))
-        .pipe(gulp.dest(publicSCSSDestination));
-};
+function publicSCSSFunction(): NodeJS.ReadWriteStream {
+  return gulp
+    .src('public-scss/*.scss')
+    .pipe(
+      sass({ outputStyle: 'compressed', includePaths: ['node_modules'] }).on(
+        'error',
+        sass.logError
+      )
+    )
+    .pipe(gulp.dest(publicSCSSDestination))
+}
 
-gulp.task("public-scss", publicSCSSFunction);
+gulp.task('public-scss', publicSCSSFunction)
 
 /*
  * Minify public/javascripts
  */
 
-const publicJavascriptsDestination = "public/javascripts";
+const publicJavascriptsDestination = 'public/javascripts'
 
-const publicJavascriptsMinFunction = () => {
+function publicJavascriptsMinFunction(): NodeJS.ReadWriteStream {
+  return gulp
+    .src('public-typescript/*.js', { allowEmpty: true })
+    .pipe(
+      changed(publicJavascriptsDestination, {
+        extension: '.min.js'
+      })
+    )
+    .pipe(minify({ noSource: true, ext: { min: '.min.js' } }))
+    .pipe(gulp.dest(publicJavascriptsDestination))
+}
 
-  return gulp.src("public-typescript/*.js", { allowEmpty: true })
-    .pipe(changed(publicJavascriptsDestination, {
-      extension: ".min.js"
-    }))
-    .pipe(minify({ noSource: true, ext: { min: ".min.js" } }))
-    .pipe(gulp.dest(publicJavascriptsDestination));
-};
+function publicJavascriptsLicenceEditFunction(): NodeJS.ReadWriteStream {
+  return gulp
+    .src(
+      [
+        'public-typescript/licence-edit/main.js',
+        'public-typescript/licence-edit/ticketTypes.js'
+      ],
+      { allowEmpty: true }
+    )
+    .pipe(concat('licence-edit.js'))
+    .pipe(minify({ noSource: true, ext: { min: '.min.js' } }))
+    .pipe(gulp.dest('public/javascripts'))
+}
 
-
-const publicJavascriptsLicenceEditFunction = () => {
-
-  return gulp.src([
-    "public-typescript/licence-edit/main.js",
-    "public-typescript/licence-edit/ticketTypes.js"
-  ], { allowEmpty: true })
-    .pipe(concat("licence-edit.js"))
-    .pipe(minify({ noSource: true, ext: { min: ".min.js" } }))
-    .pipe(gulp.dest("public/javascripts"));
-};
-
-
-gulp.task("public-javascript-min", publicJavascriptsMinFunction);
-gulp.task("public-javascript-licence-edit-min", publicJavascriptsLicenceEditFunction);
+gulp.task('public-javascript-min', publicJavascriptsMinFunction)
+gulp.task(
+  'public-javascript-licence-edit-min',
+  publicJavascriptsLicenceEditFunction
+)
 
 /*
  * Watch
  */
 
-const watchFunction = () => {
-  gulp.watch("public-scss/*.scss", publicSCSSFunction);
-  gulp.watch("public-typescript/*.js", publicJavascriptsMinFunction);
-  gulp.watch("public-typescript/licence-edit/*.js", publicJavascriptsLicenceEditFunction);
-};
+function watchFunction(): void {
+  gulp.watch('public-scss/*.scss', publicSCSSFunction)
+  gulp.watch('public-typescript/*.js', publicJavascriptsMinFunction)
+  gulp.watch(
+    'public-typescript/licence-edit/*.js',
+    publicJavascriptsLicenceEditFunction
+  )
+}
 
-gulp.task("watch", watchFunction);
+gulp.task('watch', watchFunction)
 
 /*
  * Initialize default
  */
 
-gulp.task("default", () => {
-  publicSCSSFunction();
-  publicJavascriptsMinFunction();
-  watchFunction();
-});
+gulp.task('default', () => {
+  publicSCSSFunction()
+  publicJavascriptsMinFunction()
+  watchFunction()
+})
